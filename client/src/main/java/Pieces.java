0594c89493bb6701548b29de5be85e1be50a35ff
KAJ Jacobs <kaj.jacobs@gmail.com>
2024-01-18 10:37:08 -0700

    init phase 0:added more piece class info

--- client/src/main/java/Pieces.java
+++ client/src/main/java/Pieces.java
@@ -1,10 +1,21 @@
+/**
+ * This class organizes and declares what each piece is and then what moves they make.
+ */
 public class Pieces {
-    King, Queen, Bishop, Knight, Castle, Pawn
-    init(){
-        ...
+    //King, Queen, Bishop, Knight, Castle, Pawn;
+    private String piece_name;
+    private String location;
+    private String team_color;
+
+    public Pieces(String piece_name,String location, String color) {
+        this.piece_name = piece_name;
+        this.location = location;
+        this.team_color = color;
     }
-    getPiece(){
-        ...
+
+    public String getPiece(String location){
+        //should use location to say what piece is there to help validate moves/kills and see what piece will move
+        return this.piece_name;
     }
 
 }
