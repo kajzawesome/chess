0920219b886b8f0157ce80d80d4d1e8214905778
KAJzawesome <kaj.jacobs@gmail.com>
2024-01-22 22:41:25 -0700

    moves for pawn in progress

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,11 @@
 package chess;
 
+import java.util.ArrayList;
+
+import static chess.ChessGame.TeamColor.BLACK;
+import static chess.ChessGame.TeamColor.WHITE;
+import static chess.ChessPiece.PieceType.PAWN;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -14,6 +20,36 @@ public class ChessMove {
         this.startPosition = startPosition;
         this.endPosition = endPosition;
         this.promotionPiece = promotionPiece;
+    }
+
+    public ArrayList<ChessPosition> FindMoves(ChessBoard board, ChessPiece piece, ChessPosition startPosition){
+        int currentRow = startPosition.getRow();
+        ArrayList<ChessPosition> possibleMoves = new ArrayList<ChessPosition>();
+        if (piece.getPieceType() == PAWN && piece.getTeamColor() == WHITE){
+            ChessPosition position1 = new ChessPosition(currentRow+1,startPosition.getColumn());
+            if(board.getPiece(position1) == null) {
+                possibleMoves.add(position1);
+            }
+            if (currentRow == 1){
+                ChessPosition position2 = new ChessPosition(currentRow+2,startPosition.getColumn());
+                if(board.getPiece(position2) == null){
+                    possibleMoves.add(position2);
+                }
+            }
+        }
+        if (piece.getPieceType() == PAWN && piece.getTeamColor() == BLACK){
+            ChessPosition position1 = new ChessPosition(currentRow-1,startPosition.getColumn());
+            if(board.getPiece(position1) == null) {
+                possibleMoves.add(position1);
+            }
+            if (currentRow == 6){
+                ChessPosition position2 = new ChessPosition(currentRow-2,startPosition.getColumn());
+                if(board.getPiece(position2) == null){
+                    possibleMoves.add(position2);
+                }
+            }
+        }
+        return possibleMoves;
     }
 
     /**
