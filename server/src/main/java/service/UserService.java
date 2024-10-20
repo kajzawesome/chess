db68aaa8517acc9b4cfe594c7e8e1ba6bf6d0532
Your Name <you@example.com>
2024-10-20 16:30:54 -0600

    worked on service files and record files

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -3,13 +3,15 @@ package service;
 import model.AuthData;
 import model.UserData;
 
+import java.util.UUID;
+
 public class UserService {
     public AuthData registerUser(UserData user) {
-        return new AuthData("hi", "kajzawesome");
+        return new AuthData(UUID.randomUUID().toString(), "kajzawesome");
     }
 
     public AuthData login(UserData user) {
-        return new AuthData("hi", "kajzawesome");
+        return new AuthData(UUID.randomUUID().toString(), "kajzawesome");
     }
 
     public void logout(AuthData auth) {
--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -6,4 +6,12 @@ public record AuthData(String authToken, String username) {
         this.authToken = authToken;
         this.username = username;
     }
+
+    public String getUser() {
+        return username;
+    }
+
+    public String getAuth() {
+        return authToken;
+    }
 }
