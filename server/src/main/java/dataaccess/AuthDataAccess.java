620e25dcd7b73f4b2862ac3b503fc86102c96c81
Your Name <you@example.com>
2024-10-25 19:53:29 -0600

    more done on  user and auth DAO

--- server/src/main/java/dataaccess/AuthDataAccess.java
+++ server/src/main/java/dataaccess/AuthDataAccess.java
@@ -1,4 +1,32 @@
 package dataaccess;
 
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 public class AuthDataAccess {
+    HashMap<String, UserData> validAuth  = new HashMap<String, UserData>();
+
+    public void login(UserData user) throws DataAccessException {
+        if (!alreadyLoggedIn(user)) {
+            String userNewAuth = UUID.randomUUID().toString();
+            validAuth.put(userNewAuth, user);
+        }
+        else {
+            throw new DataAccessException("Already logged in");
+        }
+    }
+
+    public boolean alreadyLoggedIn(UserData user) {
+        return validAuth.containsValue(user);
+    }
+
+    public boolean alreadyLoggedIn(String auth) {
+        return validAuth.containsKey(auth);
+    }
+
+    public void logout(String authToken) {
+        validAuth.remove(authToken);
+    }
 }
--- server/src/main/java/dataaccess/UserDataAccess.java
+++ server/src/main/java/dataaccess/UserDataAccess.java
@@ -5,8 +5,15 @@ import model.UserData;
 import java.util.HashMap;
 
 public class UserDataAccess {
-    HashMap<String, UserData> validAuth  = new HashMap<String, UserData>();
-    HashMap<String, String> users  = new HashMap<String, String>();
+    HashMap<String, UserData> users  = new HashMap<String, UserData>();
+
+    public UserData getUser(String username) {
+        return users.getOrDefault(username, null);
+    }
+
+    public void addNewUser(UserData user) {
+        users.put(user.username(), user);
+    }
 
 
 }
