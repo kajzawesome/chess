a7c1f497d206d08c9d02bfc267455a4e05c8d731
Your Name <you@example.com>
2024-10-25 19:03:31 -0600

    reworking service by making DAOs for classes

---
+++ server/src/main/java/dataaccess/UserDataAccess.java
@@ -0,0 +1,12 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.util.HashMap;
+
+public class UserDataAccess {
+    HashMap<String, UserData> validAuth  = new HashMap<String, UserData>();
+    HashMap<String, String> users  = new HashMap<String, String>();
+
+
+}
