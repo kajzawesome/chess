fe904710a6aa844ef55877f9ebdc952d8920bc4b
Your Name <you@example.com>
2024-10-30 22:25:49 -0600

    auth DAO SQL mostly done but im still partially lost

--- server/src/main/java/dataaccess/AuthDataAccessSQL.java
+++ server/src/main/java/dataaccess/AuthDataAccessSQL.java
@@ -4,7 +4,11 @@ import exception.ResponseException;
 import model.AuthData;
 import model.UserData;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class AuthDataAccessSQL {
 
@@ -12,60 +16,99 @@ public class AuthDataAccessSQL {
         configureDatabase();
     }
 
-    public AuthData login(UserData user) {
-        return null;
+    public AuthData login(UserData user) throws ResponseException {
+        var login = "INSERT INTO auth (AuthToken, User) VALUES (?, ?)";
+        var auth = executeUpdate(login, user.username());
+        return new AuthData(auth, user.username());
     }
 
-    public void logout(String authToken) {
-
+    public void logout(String authToken) throws ResponseException {
+        String logout = "DELETE FROM auth WHERE authToken=?";
+        executeUpdate(logout);
     }
 
-    public UserData getUser(String auth) throws DataAccessException, SQLException {
+    public AuthData getUser(String auth) throws DataAccessException, SQLException {
         try (var conn = DatabaseManager.getConnection()) {
-            var statement = "SELECT AuthToken, json FROM auths WHERE auth=?";
+            var statement = "SELECT AuthToken FROM auths WHERE auth=?";
             try (var ps = conn.prepareStatement(statement)) {
                 ps.setString(1, auth);
                 try (var rs = ps.executeQuery()) {
-                    if (rs.next()) { //idk if this applies to strings
-                        return new UserData(null, null, null); //idk exactly what im doing here
+                    if (rs.next()) {
+                        return readAuth(rs);
                     }
                 }
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         }
-
         return null;
     }
 
     public boolean validateAuth(String auth) {
+        var statement = String.format("SELECT AuthToken FROM auth WHERE AuthToken = '%s'", auth); //is this better than the above?
+
         return true;
     }
 
 
-    public void clearAllAuths() throws SQLException {
+    public void clearAllAuths() throws SQLException, ResponseException {
         String clear = "TRUNCATE TABLE auths";
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var ps = conn.prepareStatement(clear)) {
-                int x = 0;
-            }
-        } catch (DataAccessException e) {
-            throw new RuntimeException(e);
-        }
+        executeUpdate(clear);
     }
 
     public int loggedInUsers() {
-        //"""
-        //SELECT count(*) FROM auths
-        //"""
+        String count = "SELECT count(*) FROM auths";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(count)) {
+                ps.setString(1,count);
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return 0;
+    }
+
+    private AuthData readAuth(ResultSet rs) throws SQLException {
+        var auth = rs.getString("AuthToken");
+        var user = rs.getString("User");
+        return new AuthData(auth, user);
+    }
+
+    private String executeUpdate(String statement, Object... params) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    switch (param) {
+                        case String p -> ps.setString(i + 1, p);
+                        case Integer p -> ps.setInt(i + 1, p);
+                        case AuthData p -> ps.setString(i + 1, p.toString());
+                        case null -> ps.setNull(i + 1, NULL);
+                        default -> {
+                        }
+                    }
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getString(1);
+                }
+
+                return "";
+            }
+        } catch (SQLException e) {
+            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private final String[] createStatements = {
             """
             CREATE TABLE IF NOT EXISTS  auths (
               AuthToken String NOT NULL,
-              User UserData NOT NULL,
+              User String NOT NULL,
               PRIMARY KEY (AuthToken)
             )
             """
