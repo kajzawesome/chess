1958057a4d8c20c9a75a9dc116c30e308fc6fdc0
Your Name <you@example.com>
2024-11-09 19:26:51 -0700

    started some file build outs

---
+++ shared/src/server/ServerFacade.java
@@ -0,0 +1,16 @@
+package server;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    //register each end point
+
+}
